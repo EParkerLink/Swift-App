7226a76f3534ae136a3e3c390de61d82c7c618e2
Evan Link <evanparkerlink@gmail.com>
2017-02-21 12:14:59 -0500

    My first code

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Label01: UILabel!
+    @IBAction func button_tap(_ sender: Any) {
+        // This comment is here as an example
+        Label01.text = "Evan's Code Works"
+    }
+    @IBAction func button02_tap(_ sender: Any) {
+        Label01.text = "Evan's Other Code Works"
+    }
+    
+    var tap_count = 0
+    
+    @IBAction func button03_tap(_ sender: Any) {
+        tap_count += 1
+        if tap_count > 4{
+            Label01.text = "Congrats, you tapped the button 5 times."
+            tap_count = 0
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
